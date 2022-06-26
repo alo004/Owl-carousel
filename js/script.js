d4fc50542de6b1c25a8b643851000339097e8399
alo004 <107577821+alo004@users.noreply.github.com>
2022-06-26 10:48:01 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    dots:true,
+
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
